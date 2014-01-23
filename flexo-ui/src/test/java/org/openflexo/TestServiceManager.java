fe6fda3ef0125ef7f624f0f04f9028fb82f37fea
Sylvain Guérin <sylvain@openflexo.org>
2014-01-23 23:38:00 +0100

    MEDIUM / Attempt to fix unit tests

--- flexo-ui/src/test/java/org/openflexo/TestServiceManager.java
+++ flexo-ui/src/test/java/org/openflexo/TestServiceManager.java
@@ -6,17 +6,22 @@ import static org.junit.Assert.assertNotNull;
 import java.util.logging.Logger;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openflexo.foundation.OpenflexoTestCase;
 import org.openflexo.foundation.resource.FlexoResourceCenter;
 import org.openflexo.foundation.resource.FlexoResourceCenterService;
 import org.openflexo.foundation.technologyadapter.TechnologyAdapterService;
 import org.openflexo.module.ModuleLoader;
 import org.openflexo.module.ProjectLoader;
+import org.openflexo.test.OrderedRunner;
+import org.openflexo.test.TestOrder;
 import org.openflexo.view.controller.TechnologyAdapterControllerService;
 
+@RunWith(OrderedRunner.class)
 public class TestServiceManager extends OpenflexoTestCase {
 
-	protected static final Logger logger = Logger.getLogger(TestServiceManager.class.getPackage().getName());
+	protected static final Logger logger = Logger
+			.getLogger(TestServiceManager.class.getPackage().getName());
 
 	private static ApplicationContext testApplicationContext;
 	private static FlexoResourceCenter resourceCenter;
@@ -25,18 +30,24 @@ public class TestServiceManager extends OpenflexoTestCase {
 	 * Instanciate test ApplicationContext
 	 */
 	@Test
+	@TestOrder(1)
 	public void test0UseTestApplicationContext() {
 		log("test0UseTestApplicationContext()");
 		testApplicationContext = new TestApplicationContext();
-		resourceCenter = testApplicationContext.getResourceCenterService().getResourceCenters().get(0);
+		resourceCenter = testApplicationContext.getResourceCenterService()
+				.getResourceCenters().get(0);
 
-		logger.info("services: " + testApplicationContext.getRegisteredServices());
+		logger.info("services: "
+				+ testApplicationContext.getRegisteredServices());
 
 		assertNotNull(testApplicationContext.getService(ProjectLoader.class));
 		assertNotNull(testApplicationContext.getService(ModuleLoader.class));
-		assertNotNull(testApplicationContext.getService(FlexoResourceCenterService.class));
-		assertNotNull(testApplicationContext.getService(TechnologyAdapterService.class));
-		assertNotNull(testApplicationContext.getService(TechnologyAdapterControllerService.class));
+		assertNotNull(testApplicationContext
+				.getService(FlexoResourceCenterService.class));
+		assertNotNull(testApplicationContext
+				.getService(TechnologyAdapterService.class));
+		assertNotNull(testApplicationContext
+				.getService(TechnologyAdapterControllerService.class));
 
 	}
 
@@ -44,20 +55,19 @@ public class TestServiceManager extends OpenflexoTestCase {
 	 * Try to load a module
 	 */
 	@Test
+	@TestOrder(2)
 	public void test1ModuleLoading() {
 		ModuleLoader moduleLoader = testApplicationContext.getModuleLoader();
-		assertEquals(moduleLoader, testApplicationContext.getService(ModuleLoader.class));
+		assertEquals(moduleLoader,
+				testApplicationContext.getService(ModuleLoader.class));
 
-		/*UserType.setCurrentUserType(UserType.MAINTAINER);
-
-		try {
-			ExternalModule loadedModule = moduleLoader.getVPMModuleInstance();
-			if (loadedModule != null) {
-				fail();
-			}
-			// This module is not in the classpath, normal
-		} catch (ModuleLoadingException e) {
-			fail();
-		}*/
+		/*
+		 * UserType.setCurrentUserType(UserType.MAINTAINER);
+		 * 
+		 * try { ExternalModule loadedModule =
+		 * moduleLoader.getVPMModuleInstance(); if (loadedModule != null) {
+		 * fail(); } // This module is not in the classpath, normal } catch
+		 * (ModuleLoadingException e) { fail(); }
+		 */
 	}
 }
