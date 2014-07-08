48b02bb30f2788af994a9362e83c88973af9817e
xtof <xtof-osd@bothrops.org>
2014-07-08 08:38:31 +0200

    LOW: removed logs

--- flexo-ui/src/main/java/org/openflexo/view/controller/ResourceCenterEditor.java
+++ flexo-ui/src/main/java/org/openflexo/view/controller/ResourceCenterEditor.java
@@ -19,81 +19,80 @@ import org.openflexo.view.FlexoFrame;
 
 public class ResourceCenterEditor implements HasPropertyChangeSupport {
 
-	static final Logger logger = Logger.getLogger(ResourceCenterEditor.class.getPackage().getName());
+    static final Logger logger = Logger.getLogger(ResourceCenterEditor.class.getPackage().getName());
 
-	public static final String RESOURCE_CENTER_EDITOR_FIB_NAME = "Fib/ResourceCenterEditor.fib";
+    public static final String RESOURCE_CENTER_EDITOR_FIB_NAME = "Fib/ResourceCenterEditor.fib";
 
-	private final PropertyChangeSupport _pcSupport;
+    private final PropertyChangeSupport _pcSupport;
 
-	private final FlexoResourceCenterService rcService;
+    private final FlexoResourceCenterService rcService;
 
-	public ResourceCenterEditor(FlexoResourceCenterService rcService) {
-		_pcSupport = new PropertyChangeSupport(this);
-		this.rcService = rcService;
-	}
+    public ResourceCenterEditor(FlexoResourceCenterService rcService) {
+        _pcSupport = new PropertyChangeSupport(this);
+        this.rcService = rcService;
+    }
 
-	public FlexoResourceCenterService getRcService() {
-		return rcService;
-	}
+    public FlexoResourceCenterService getRcService() {
+        return rcService;
+    }
 
-	@Override
-	public String getDeletedProperty() {
-		return null;
-	}
+    @Override
+    public String getDeletedProperty() {
+        return null;
+    }
 
-	@Override
-	public PropertyChangeSupport getPropertyChangeSupport() {
-		return _pcSupport;
-	}
+    @Override
+    public PropertyChangeSupport getPropertyChangeSupport() {
+        return _pcSupport;
+    }
 
-	public void addResourceCenter() {
-		System.out.println("Add resource center ");
-		FIBComponent askRCDirectoryComponent = FIBLibrary.instance().retrieveFIBComponent(AskResourceCenterDirectory.FIB_FILE);
-		AskResourceCenterDirectory askDir = new AskResourceCenterDirectory();
-		FIBDialog dialog = FIBDialog.instanciateAndShowDialog(askRCDirectoryComponent, askDir, FlexoFrame.getActiveFrame(), true,
-				FlexoLocalization.getMainLocalizer());
-		if (dialog.getStatus() == Status.VALIDATED) {
-			DirectoryResourceCenter newRC = new DirectoryResourceCenter(askDir.getLocalResourceDirectory());
-			showProgress("scanning_resources");
-			rcService.addToResourceCenters(newRC);
-			hideProgress();
-		}
-	}
+    public void addResourceCenter() {
+        FIBComponent askRCDirectoryComponent = FIBLibrary.instance().retrieveFIBComponent(AskResourceCenterDirectory.FIB_FILE);
+        AskResourceCenterDirectory askDir = new AskResourceCenterDirectory();
+        FIBDialog dialog = FIBDialog.instanciateAndShowDialog(askRCDirectoryComponent, askDir, FlexoFrame.getActiveFrame(), true,
+                FlexoLocalization.getMainLocalizer());
+        if (dialog.getStatus() == Status.VALIDATED) {
+            DirectoryResourceCenter newRC = new DirectoryResourceCenter(askDir.getLocalResourceDirectory());
+            showProgress("scanning_resources");
+            rcService.addToResourceCenters(newRC);
+            hideProgress();
+        }
+    }
 
-	public void removeResourceCenter(FlexoResourceCenter rc) {
-		System.out.println("Removing resources" + rc);
-		if (rc instanceof DirectoryResourceCenter) {
-			showProgress("removing_resources");
-			rcService.removeFromResourceCenters(rc);
-			hideProgress();
-		}
-	}
+    public void removeResourceCenter(FlexoResourceCenter rc) {
+        System.out.println("Removing resources" + rc);
+        if (rc instanceof DirectoryResourceCenter) {
+            showProgress("removing_resources");
+            rcService.removeFromResourceCenters(rc);
+            hideProgress();
+        }
+    }
 
-	public void refreshResourceCenter(FlexoResourceCenter rc) {
-		if (rc != null) {
-			System.out.println("Scanning resources " + rc);
-			try {
-				showProgress("scanning_resources");
-				rc.update();
-				hideProgress();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public void refreshResourceCenter(FlexoResourceCenter rc) {
+        if (rc != null) {
+            System.out.println("Scanning resources " + rc);
+            try {
+                showProgress("scanning_resources");
+                rc.update();
+                hideProgress();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	private void showProgress(String stepname) {
-		ProgressWindow.showProgressWindow(FlexoLocalization.localizedForKey(stepname), 1);
-		ProgressWindow.instance().setProgress(FlexoLocalization.localizedForKey(stepname));
-	}
+    private void showProgress(String stepname) {
+        ProgressWindow.showProgressWindow(FlexoLocalization.localizedForKey(stepname), 1);
+        ProgressWindow.instance().setProgress(FlexoLocalization.localizedForKey(stepname));
+    }
 
-	private void hideProgress() {
-		ProgressWindow.hideProgressWindow();
-	}
+    private void hideProgress() {
+        ProgressWindow.hideProgressWindow();
+    }
 
-	public void saveResourceCenters() {
-		System.out.println("Save resource centers");
-		rcService.storeDirectoryResourceCenterLocations();
-	}
+    public void saveResourceCenters() {
+        System.out.println("Save resource centers");
+        rcService.storeDirectoryResourceCenterLocations();
+    }
 
 }
