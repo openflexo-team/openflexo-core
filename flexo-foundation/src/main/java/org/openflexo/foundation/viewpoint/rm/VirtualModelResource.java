ee31c5e0078bf97f78c8aba8281f94567a9b5366
Vincent <vincent@openflexo.org>
2014-10-20 19:32:38 +0200

    MEDIUM / [FlexoIODelegate] Inherit from Directory Container resource

--- flexo-foundation/src/main/java/org/openflexo/foundation/viewpoint/rm/VirtualModelResource.java
+++ flexo-foundation/src/main/java/org/openflexo/foundation/viewpoint/rm/VirtualModelResource.java
@@ -1,28 +1,23 @@
 package org.openflexo.foundation.viewpoint.rm;
 
-import java.io.File;
-
-import javax.xml.bind.annotation.XmlAttribute;
-
+import org.openflexo.foundation.resource.DirectoryContainerResource;
 import org.openflexo.foundation.resource.PamelaResource;
 import org.openflexo.foundation.technologyadapter.TechnologyAdapterResource;
 import org.openflexo.foundation.viewpoint.VirtualModel;
 import org.openflexo.foundation.viewpoint.VirtualModelModelFactory;
 import org.openflexo.foundation.viewpoint.VirtualModelTechnologyAdapter;
-import org.openflexo.model.annotations.Getter;
 import org.openflexo.model.annotations.ImplementationClass;
 import org.openflexo.model.annotations.ModelEntity;
-import org.openflexo.model.annotations.Setter;
 import org.openflexo.model.annotations.XMLElement;
 
 @ModelEntity
 @ImplementationClass(VirtualModelResourceImpl.class)
 @XMLElement
 public interface VirtualModelResource extends PamelaResource<VirtualModel, VirtualModelModelFactory>,
-		TechnologyAdapterResource<VirtualModel, VirtualModelTechnologyAdapter> {
+		TechnologyAdapterResource<VirtualModel, VirtualModelTechnologyAdapter>, DirectoryContainerResource<VirtualModel> {
 
 	public static final String VIEW_POINT_LIBRARY = "viewPointLibrary";
-	public static final String DIRECTORY = "directory";
+	//public static final String DIRECTORY = "directory";
 
 	/**
 	 * Return virtual model stored by this resource<br>
@@ -42,12 +37,12 @@ public interface VirtualModelResource extends PamelaResource<VirtualModel, Virtu
 	@Setter(VIEW_POINT_LIBRARY)
 	public void setViewPointLibrary(ViewPointLibrary viewPointLibrary);*/
 
-	@Getter(DIRECTORY)
+	/*@Getter(DIRECTORY)
 	@XmlAttribute
 	public File getDirectory();
 
 	@Setter(DIRECTORY)
-	public void setDirectory(File file);
+	public void setDirectory(File file);*/
 
 	@Override
 	public ViewPointResource getContainer();
