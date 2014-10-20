187d3687de6f71ecfb1ac92dcb32a81527ded96a
Vincent <vincent@openflexo.org>
2014-10-20 18:10:12 +0200

    MEDIUM / [FlexoIODelegate] Add an abstract Flexo IO Delegate to flexo resources in order to manage the serialization concern.

---
+++ flexo-foundation/src/main/java/org/openflexo/foundation/resource/FlexoIODelegate.java
@@ -0,0 +1,72 @@
+package org.openflexo.foundation.resource;
+
+import org.openflexo.model.annotations.Getter;
+import org.openflexo.model.annotations.Implementation;
+import org.openflexo.model.annotations.ModelEntity;
+import org.openflexo.model.annotations.PropertyIdentifier;
+import org.openflexo.model.annotations.Setter;
+import org.openflexo.model.annotations.XMLElement;
+
+/**
+ * Flexo IO Delegate makes a link between a flexo resource and a serialization artefact.
+ * @author Vincent, Sylvain
+ *
+ * @param <I>
+ */
+@ModelEntity
+@XMLElement
+public interface FlexoIODelegate<I> {
+
+	@PropertyIdentifier(type = String.class)
+	public static final String NAME = "name";
+	
+	@PropertyIdentifier(type = String.class)
+	public static final String FLEXO_RESOURCE = "flexo_resource";
+	
+	/**
+	 *  A Serialization Artefact represents the flexo resource according to a given serialization type
+	 */
+	public static final String SERIALIZATION_ARTEFACT = "serialization_artefact";
+
+	@Getter(value= FLEXO_RESOURCE, inverse = FlexoResource.FLEXO_IO_DELEGATE)
+	public FlexoResource<?> getFlexoResource();
+
+	@Setter(FLEXO_RESOURCE)
+	public void setFlexoResource(FlexoResource<?> resource);
+	
+	@Getter(value= SERIALIZATION_ARTEFACT, ignoreType =true)
+	public I getSerializationArtefact();
+
+	@Setter(SERIALIZATION_ARTEFACT)
+	public void setSerializationArtefact(I artefact);
+
+	/**
+	 * Indicates whether this resource can be edited or not. Returns <code>true</code> if the resource cannot be edited, else returns
+	 * <code>false</code>.
+	 * 
+	 * @return <code>true</code> if the resource cannot be edited, else returns <code>false</code>.
+	 */
+	public boolean isReadOnly();
+	
+	public boolean delete();
+	
+	/**
+	 * Return true if the serialization artefact exists
+	 * @return
+	 */
+	public boolean exists();
+	
+	/**
+	 * Return true if the serialization artefact can be overrided
+	 * @return
+	 */
+	public boolean hasWritePermission();
+	
+	public abstract String toString();
+	
+	public FileWritingLock willWriteOnDisk();
+	
+	public void hasWrittenOnDisk(FileWritingLock lock);
+	
+	public void notifyHasBeenWrittenOnDisk();
+}
