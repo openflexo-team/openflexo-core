1f495c53dab77c57833edb79db509d02c4f8d573
Sylvain Guérin <sylvain@openflexo.org>
2014-11-05 14:07:47 +0100

    LOW / Fixed CORE-35

--- flexo-ui/src/main/java/org/openflexo/components/SaveProjectsDialog.java
+++ flexo-ui/src/main/java/org/openflexo/components/SaveProjectsDialog.java
@@ -20,15 +20,15 @@
 package org.openflexo.components;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.openflexo.fib.controller.FIBController.Status;
 import org.openflexo.fib.controller.FIBDialog;
 import org.openflexo.foundation.FlexoProject;
 import org.openflexo.localization.FlexoLocalization;
-import org.openflexo.rm.ResourceLocator;
 import org.openflexo.rm.Resource;
+import org.openflexo.rm.ResourceLocator;
+import org.openflexo.toolbox.PropertyChangedSupportDefaultImplementation;
 import org.openflexo.view.FlexoFrame;
 import org.openflexo.view.controller.FlexoController;
 import org.openflexo.view.controller.FlexoFIBController;
@@ -39,13 +39,11 @@ import org.openflexo.view.controller.FlexoFIBController;
  */
 public class SaveProjectsDialog {
 
-	
-	
 	public static final Resource FIB_FILE_NAME = ResourceLocator.locateResource("Fib/SaveProjects.fib");
-	private ProjectList data;
+	private final ProjectList data;
 
-	public static class ProjectList {
-		private List<FlexoProject> projects;
+	public static class ProjectList extends PropertyChangedSupportDefaultImplementation {
+		private final List<FlexoProject> projects;
 		private List<FlexoProject> selected;
 
 		public ProjectList(List<FlexoProject> projects) {
@@ -62,15 +60,27 @@ public class SaveProjectsDialog {
 		}
 
 		public void setSelected(List<FlexoProject> selected) {
+			List<FlexoProject> old = this.selected;
 			this.selected = selected;
+			getPropertyChangeSupport().firePropertyChange("selected", old, selected);
 		}
 
 		public void selectAll() {
-			setSelected(getProjects());
+			for (FlexoProject p : getProjects()) {
+				if (!selected.contains(p)) {
+					selected.add(p);
+					getPropertyChangeSupport().firePropertyChange("selected", null, p);
+				}
+			}
 		}
 
 		public void deselectAll() {
-			setSelected(Collections.<FlexoProject> emptyList());
+			for (FlexoProject p : getProjects()) {
+				if (selected.contains(p)) {
+					selected.remove(p);
+					getPropertyChangeSupport().firePropertyChange("selected", p, null);
+				}
+			}
 		}
 	}
 
