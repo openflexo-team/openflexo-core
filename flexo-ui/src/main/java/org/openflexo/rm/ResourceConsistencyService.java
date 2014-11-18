d1c319b7e5b93a171ee3ea46124818852d29abc3
Vincent <vincent@openflexo.org>
2014-11-18 16:31:59 +0100

    MEDIUM / Remove uri management dialog when uri conflict is detected. Just inform the user of possible problems in there set of resources.

--- flexo-ui/src/main/java/org/openflexo/rm/ResourceConsistencyService.java
+++ flexo-ui/src/main/java/org/openflexo/rm/ResourceConsistencyService.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.openflexo.foundation.DataModification;
 import org.openflexo.foundation.FlexoService;
 import org.openflexo.foundation.FlexoServiceImpl;
+import org.openflexo.foundation.resource.FileFlexoIODelegate;
 import org.openflexo.foundation.resource.FlexoResource;
 import org.openflexo.foundation.resource.MissingFlexoResource;
-import org.openflexo.foundation.resource.ResourceData;
-import org.openflexo.foundation.resource.ResourceLoaded;
 import org.openflexo.foundation.resource.ResourceManager;
 import org.openflexo.foundation.resource.ResourceRegistered;
 import org.openflexo.foundation.resource.ResourceUnregistered;
@@ -27,7 +25,10 @@ import org.openflexo.view.controller.FlexoController;
  */
 public class ResourceConsistencyService extends FlexoServiceImpl {
 	
+	// The whole set of conflicts
 	private List<ConflictedResourceSet> conflictedResourceSets;
+	
+	private List<ViewResource> viewsWithoutViewpoint;
 	
 	public ResourceConsistencyService() {
 	}
@@ -36,36 +37,71 @@ public class ResourceConsistencyService extends FlexoServiceImpl {
 	public void initialize() {
 		// Initialize current conflicted resource set
 		conflictedResourceSets = getConflictedResourceSets();
-		informOfConflictedResourceSets();
+		// Inform the user of conflicts
+		informOfConflictedResourceSets(conflictedResourceSets);
+		
+		viewsWithoutViewpoint = new ArrayList<ViewResource>();
 	}
-
+	
 	/**
 	 * Receive a ntification that a new resource has changed.
 	 * It send a message if a conflict exists or resource not complete
 	 */
 	@Override
 	public void receiveNotification(FlexoService caller, ServiceNotification notification) {
+		// A resource is unregistered or registered
 		if (notification instanceof ResourceUnregistered){
 			ResourceUnregistered unRegistered = (ResourceUnregistered)notification;
-			informOfConflictedResourceSets();
-			if(unRegistered.newValue() instanceof ViewResourceImpl){
-				ViewResourceImpl viewResource = (ViewResourceImpl)unRegistered.newValue();
-				if(viewResource.getViewPointResource()==null){
-					informOfViewPointMissing(viewResource);
-				}
-			}
+			checkProblems(unRegistered.newValue());
 		}else if(notification instanceof ResourceRegistered){
 			ResourceRegistered registered = (ResourceRegistered)notification;
-			informOfConflictedResourceSets();
-			if(registered.newValue() instanceof ViewResourceImpl){
-				ViewResourceImpl viewResource = (ViewResourceImpl)registered.newValue();
-				if(viewResource.getViewPointResource()==null){
-					informOfViewPointMissing(viewResource);
-				}
-			}
+			checkProblems(registered.newValue());
 		}
 	}
 	
+	private void checkProblems(Object value){
+		if(value instanceof FlexoResource<?>){
+			informOfURIConflict((FlexoResource<?>) value);
+		}
+		if(value instanceof ViewResourceImpl){
+			informOfViewpointMissing((ViewResourceImpl) value);
+		}
+	}
+	
+	private void informOfURIConflict(FlexoResource<?> resource){
+		ConflictedResourceSet conflict = getConflictedResourceSet(resource);
+		if(conflict!=null){
+			informOfConflictedResourceSet(conflict);
+		}
+	}
+	
+	/**
+	 * Infor of a viewpoint missing for a view. Inform once.
+	 * @param viewResource
+	 */
+	private void informOfViewpointMissing(ViewResourceImpl viewResource){
+		if(viewResource.getViewPointResource()==null && !viewsWithoutViewpoint.contains(viewResource)){
+			informOfViewPointMissing(viewResource);
+			viewsWithoutViewpoint.add(viewResource);
+		}
+	}
+	
+	private ConflictedResourceSet getConflictedResourceSet(FlexoResource<?> resource){
+		// If there is many resource with the same URI
+		if(multipleResourcesWithSameURI(resource)){
+			// If a set with this URI exists, add this resource to this set
+			ConflictedResourceSet conflictedSet = getConflictedResourceSet(resource.getURI());
+			// Otherwize create a new set
+			if(conflictedSet==null){
+				conflictedSet = new ConflictedResourceSet(getResources(resource.getURI()));
+				conflictedResourceSets.add(conflictedSet);
+			}else if(!conflictedSet.getConflictedResources().contains(resource)){
+				conflictedSet.getConflictedResources().add(resource);
+			}
+			return conflictedSet;
+		}
+		return null;
+	}
 	
 	/**
 	 * Get the set of conflicted sets
@@ -77,17 +113,7 @@ public class ResourceConsistencyService extends FlexoServiceImpl {
 		}
 		// Browse all resources
 		for(FlexoResource<?> resource : getResourceManager().getRegisteredResources()){
-			// If there is many resource with the same URI
-			if(multipleResourcesWithSameURI(resource)){
-				// If a set with this URI exists, add this resource to this set
-				ConflictedResourceSet conflictedSet = getConflictedResourceSet(resource.getURI());
-				// Otherwize create a new set
-				if(conflictedSet==null){
-					conflictedResourceSets.add(new ConflictedResourceSet(resource));
-				}else if(!conflictedSet.getConflictedResources().contains(resource)){
-					conflictedSet.getConflictedResources().add(resource);
-				}
-			}
+			getConflictedResourceSet(resource);
 		}
 		return conflictedResourceSets;
 	}
@@ -120,24 +146,37 @@ public class ResourceConsistencyService extends FlexoServiceImpl {
 		}
 	}
 	
-	public void removeConflictedResourceSet(ConflictedResourceSet conflictedResourceSet){
+	//TODO
+	/*public void removeConflictedResourceSet(ConflictedResourceSet conflictedResourceSet){
 		conflictedResourceSets.remove(conflictedResourceSet);
-	}
+	}*/
 	
-	private void informOfConflictedResourceSets(){
-		if(getNumberOfConflicts()>0){
-			if(FlexoController.confirm(Integer.toString(getNumberOfConflicts()) + " URI conflicts have been found. Would you like to manage them?")){
-				// Browse all set of conflicted resources
-				List<ConflictedResourceSet> resourcesToRemove = new ArrayList<ConflictedResourceSet>();
-				for(ConflictedResourceSet conflictedResourceSet : getConflictedResourceSets()){
-					manageConflictedResourceSet(conflictedResourceSet);
-					resourcesToRemove.add(conflictedResourceSet);
-				}
-				for(ConflictedResourceSet conflictedResourceSet :  resourcesToRemove){
-					removeConflictedResourceSet(conflictedResourceSet);
+	private void informOfConflictedResourceSets(List<ConflictedResourceSet> conflicts){
+		if(conflicts.size()>0){
+			StringBuilder conflictsInformation = new StringBuilder();
+			for(ConflictedResourceSet conflict : conflicts){
+				conflictsInformation.append("URI : " + conflict.getCommonUri() +" is owned by \n");
+				for(FlexoResource<?> fr : conflict.getConflictedResources()){
+					conflictsInformation.append(fr.getRelativePath());
 				}
 			}
+			FlexoController.notify(Integer.toString(conflicts.size()) + " URI conflicts have been found:\n" +
+					conflictsInformation.toString());
 		}
+	}
+	
+	private void informOfConflictedResourceSet(ConflictedResourceSet conflict){
+		StringBuilder sb = new StringBuilder();
+		sb.append("URI : " + conflict.getCommonUri() +" is owned by \n");
+		for(FlexoResource<?> fr : conflict.getConflictedResources()){
+			if(fr.getFlexoIODelegate() instanceof FileFlexoIODelegate){
+				sb.append(((FileFlexoIODelegate)fr.getFlexoIODelegate()).getFile().getAbsolutePath());
+			}else{
+				sb.append(fr.getFlexoIODelegate().toString());
+			}
+			sb.append("\n");
+		}
+		FlexoController.notify(" URI conflicts have been found:\n" + sb.toString());
 	}
 	
 	/**
