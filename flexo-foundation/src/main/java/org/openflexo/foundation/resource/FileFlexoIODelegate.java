87edd38dc3ce55fdb8b11feee4b02094d920e1f9
Sylvain Guérin <sylvain@openflexo.org>
2014-12-08 13:03:30 +0100

    MEDIUM / Fixed NPE

--- flexo-foundation/src/main/java/org/openflexo/foundation/resource/FileFlexoIODelegate.java
+++ flexo-foundation/src/main/java/org/openflexo/foundation/resource/FileFlexoIODelegate.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.jar.JarInputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,7 +20,7 @@ import org.openflexo.toolbox.FileUtils;
 @ModelEntity
 @XMLElement
 public interface FileFlexoIODelegate extends FlexoIOStreamDelegate<File> {
-	
+
 	public static final String FILE = "file";
 
 	@Getter(FILE)
@@ -34,23 +33,23 @@ public interface FileFlexoIODelegate extends FlexoIOStreamDelegate<File> {
 	public boolean renameFileTo(String name) throws InvalidFileNameException, IOException;
 
 	public boolean delete(boolean deleteFile);
-	
+
 	@Implementation
-	public abstract class FileFlexoIODelegateImpl extends FlexoIOStreamDelegateImpl<File> implements FileFlexoIODelegate{
-		
+	public abstract class FileFlexoIODelegateImpl extends FlexoIOStreamDelegateImpl<File> implements FileFlexoIODelegate {
+
 		private final Logger logger = Logger.getLogger(FileFlexoIODelegateImpl.class.getPackage().getName());
-		
-		public static FileFlexoIODelegate makeFileFlexoIODelegate(File file, ModelFactory factory){
-			FileFlexoIODelegate fileIODelegate = factory.newInstance(FileFlexoIODelegate.class) ;
+
+		public static FileFlexoIODelegate makeFileFlexoIODelegate(File file, ModelFactory factory) {
+			FileFlexoIODelegate fileIODelegate = factory.newInstance(FileFlexoIODelegate.class);
 			fileIODelegate.setFile(file);
 			return fileIODelegate;
 		}
-		
+
 		@Override
 		public File getSerializationArtefact() {
 			return getFile();
 		}
-		
+
 		@Override
 		public synchronized boolean hasWritePermission() {
 			return getFile() == null || (!getFile().exists() || getFile().canWrite()) && getFile().getParentFile() != null
@@ -69,7 +68,7 @@ public interface FileFlexoIODelegate extends FlexoIOStreamDelegate<File> {
 			}
 			return true;
 		}
-		
+
 		/**
 		 * Delete this resource by deleting the file
 		 */
@@ -106,39 +105,42 @@ public interface FileFlexoIODelegate extends FlexoIOStreamDelegate<File> {
 				notifyHasBeenWrittenOnDisk();
 			}
 		}
-		
+
 		/**
 		 * Delete this resource. Delete file is flag deleteFile is true.
 		 */
 		@Override
 		public boolean delete(boolean deleteFile) {
 			if (hasWritePermission()) {
-				//if (getFlexoResource().delete()) {
-					if (getFile() != null && getFile().exists() && deleteFile) {
-						getFlexoResource().getServiceManager().getResourceManager().addToFilesToDelete(getFile());
-						if (logger.isLoggable(Level.INFO)) {
-							logger.info("Will delete file " + getFile().getAbsolutePath() + " upon next save of RM");
-						}
+				// if (getFlexoResource().delete()) {
+				if (getFile() != null && getFile().exists() && deleteFile) {
+					getFlexoResource().getServiceManager().getResourceManager().addToFilesToDelete(getFile());
+					if (logger.isLoggable(Level.INFO)) {
+						logger.info("Will delete file " + getFile().getAbsolutePath() + " upon next save of RM");
 					}
-					return true;
-				//}
-				//return false;
+				}
+				return true;
+				// }
+				// return false;
 			} else {
 				logger.warning("Delete requested for READ-ONLY file resource " + this);
 				return false;
 			}
 		}
-		
+
 		@Override
 		public boolean exists() {
+			if (getFile() == null) {
+				return false;
+			}
 			return getFile().exists();
 		}
-		
+
 		@Override
-		public String toString(){
+		public String toString() {
 			return getFile().getAbsolutePath();
 		}
-		
+
 		@Override
 		public InputStream getInputStream() {
 			try {
@@ -147,11 +149,9 @@ public interface FileFlexoIODelegate extends FlexoIOStreamDelegate<File> {
 				e.printStackTrace();
 				return null;
 			}
-	
+
 		}
-		
 
 	}
-	
-	
+
 }
