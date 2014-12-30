f9a2b335eb17e4f871e9104d828d801a6a02a44e
Sylvain Guérin <sylvain@openflexo.org>
2014-12-30 18:14:36 +0100

    MEDIUM / FML grammar / WIP

--- fml-parser/src/main/java/org/openflexo/foundation/fml/parser/FMLSemanticsAnalyzer.java
+++ fml-parser/src/main/java/org/openflexo/foundation/fml/parser/FMLSemanticsAnalyzer.java
@@ -1,8 +1,11 @@
 package org.openflexo.foundation.fml.parser;
 
 import org.openflexo.foundation.fml.parser.analysis.DepthFirstAdapter;
-import org.openflexo.foundation.fml.parser.node.AMainDeclaration;
+import org.openflexo.foundation.fml.parser.node.AImportDeclaration;
 import org.openflexo.foundation.fml.parser.node.ANamespaceDeclaration;
+import org.openflexo.foundation.fml.parser.node.AUseDeclaration;
+import org.openflexo.foundation.fml.parser.node.AViewpointDeclaration;
+import org.openflexo.foundation.fml.parser.node.AVirtualModelDeclaration;
 
 /**
  * This class implements the semantics analyzer for a parsed FML compilation unit.<br>
@@ -17,14 +20,36 @@ class FMLSemanticsAnalyzer extends DepthFirstAdapter {
 	}
 
 	@Override
-	public void caseANamespaceDeclaration(ANamespaceDeclaration node) {
-		super.caseANamespaceDeclaration(node);
+	public void outANamespaceDeclaration(ANamespaceDeclaration node) {
+		super.outANamespaceDeclaration(node);
 		System.out.println("Tiens, un namespace: " + node);
 	}
 
 	@Override
-	public void caseAMainDeclaration(AMainDeclaration node) {
-		super.caseAMainDeclaration(node);
-		System.out.println("Tiens, une MainDeclaration: " + node);
+	public void outAUseDeclaration(AUseDeclaration node) {
+		super.outAUseDeclaration(node);
+		System.out.println("Tiens, un use: " + node + " ta=" + node.getTechnologyAdapter()
+				+ node.getTechnologyAdapterAdditionalIdentifiers() + " id=" + node.getTaId());
 	}
+
+	@Override
+	public void outAImportDeclaration(AImportDeclaration node) {
+		super.outAImportDeclaration(node);
+		System.out.println("Tiens, un import: " + node);
+	}
+
+	@Override
+	public void outAVirtualModelDeclaration(AVirtualModelDeclaration node) {
+		super.outAVirtualModelDeclaration(node);
+		System.out.println("Tiens, un VirtualModelDeclaration: " + node);
+		System.out.println("Annotations = " + node.getAnnotations());
+	}
+
+	@Override
+	public void outAViewpointDeclaration(AViewpointDeclaration node) {
+		super.outAViewpointDeclaration(node);
+		System.out.println("Tiens, un ViewPointDeclaration: " + node);
+		System.out.println("Annotations = " + node.getAnnotations());
+	}
+
 }
