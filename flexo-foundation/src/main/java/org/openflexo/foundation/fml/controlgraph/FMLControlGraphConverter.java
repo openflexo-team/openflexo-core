3356d324b2e18d48216ffe3e07f825960e81f12f
Sylvain Guérin <sylvain@openflexo.org>
2015-01-12 09:13:15 +0100

    MEDIUM / Convert old EditionAction to new EditionAction hierarchy

--- flexo-foundation/src/main/java/org/openflexo/foundation/fml/controlgraph/FMLControlGraphConverter.java
+++ flexo-foundation/src/main/java/org/openflexo/foundation/fml/controlgraph/FMLControlGraphConverter.java
@@ -1,5 +1,10 @@
 package org.openflexo.foundation.fml.controlgraph;
 
+import org.apache.commons.lang3.StringUtils;
+import org.openflexo.foundation.fml.editionaction.AssignableAction;
+import org.openflexo.foundation.fml.editionaction.AssignationAction;
+import org.openflexo.foundation.fml.editionaction.DeclarationAction;
+import org.openflexo.foundation.fml.editionaction.DeclareFlexoRole;
 import org.openflexo.foundation.fml.editionaction.EditionAction;
 
 /**
@@ -12,6 +17,32 @@ import org.openflexo.foundation.fml.editionaction.EditionAction;
 public class FMLControlGraphConverter {
 
 	public static void addToActions(FMLControlGraphOwner owner, String ownerContext, EditionAction anAction) {
+
+		if (anAction instanceof DeclareFlexoRole) {
+			DeclareFlexoRole declareFlexoRole = (DeclareFlexoRole) anAction;
+			AssignationAction action = owner.getFMLModelFactory().newAssignationAction(declareFlexoRole.getObject());
+			action.initializeDeserialization(owner.getFMLModelFactory());
+			action.getAssignableAction().initializeDeserialization(owner.getFMLModelFactory());
+			action.setAssignation(declareFlexoRole.getDeprecatedAssignation());
+			anAction = action;
+		}
+		if (anAction instanceof AssignableAction && !(anAction instanceof AssignationAction)) {
+			AssignableAction assignableAction = (AssignableAction) anAction;
+			if (StringUtils.isNotEmpty(assignableAction.getDeprecatedVariableName())) {
+				DeclarationAction action = owner.getFMLModelFactory().newDeclarationAction(assignableAction.getDeprecatedVariableName(),
+						assignableAction);
+				action.initializeDeserialization(owner.getFMLModelFactory());
+				action.getAssignableAction().initializeDeserialization(owner.getFMLModelFactory());
+				anAction = action;
+			} else if (assignableAction.getDeprecatedAssignation() != null && assignableAction.getDeprecatedAssignation().isSet()) {
+				AssignationAction action = owner.getFMLModelFactory().newAssignationAction(assignableAction);
+				action.initializeDeserialization(owner.getFMLModelFactory());
+				action.getAssignableAction().initializeDeserialization(owner.getFMLModelFactory());
+				action.setAssignation(assignableAction.getDeprecatedAssignation());
+				anAction = action;
+			}
+		}
+
 		FMLControlGraph controlGraph = owner.getControlGraph(ownerContext);
 		if (controlGraph == null) {
 			// If control graph is null, action will be new new control graph
