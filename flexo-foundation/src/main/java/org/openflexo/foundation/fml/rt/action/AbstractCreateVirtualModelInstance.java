a2e8495ec0985fba85ca5c9c31eed50663828b89
Sylvain Guérin <sylvain@openflexo.org>
2016-09-06 10:06:25 +0200

    LOW / Format

--- flexo-foundation/src/main/java/org/openflexo/foundation/fml/rt/action/AbstractCreateVirtualModelInstance.java
+++ flexo-foundation/src/main/java/org/openflexo/foundation/fml/rt/action/AbstractCreateVirtualModelInstance.java
@@ -80,7 +80,7 @@ import org.openflexo.toolbox.StringUtils;
  *            type of container of newly created AbstractVirtualModelInstance
  */
 public abstract class AbstractCreateVirtualModelInstance<A extends AbstractCreateVirtualModelInstance<A, T, VMI, VM>, T extends FlexoObject, VMI extends AbstractVirtualModelInstance<VMI, VM>, VM extends AbstractVirtualModel<VM>>
-		extends FlexoAction<A, T, FlexoObject>implements FlexoObserver {
+		extends FlexoAction<A, T, FlexoObject> implements FlexoObserver {
 
 	private static final Logger logger = Logger.getLogger(AbstractCreateVirtualModelInstance.class.getPackage().getName());
 
