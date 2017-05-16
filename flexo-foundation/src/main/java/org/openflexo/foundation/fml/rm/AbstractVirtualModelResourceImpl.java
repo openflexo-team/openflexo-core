8e73372988996c8f4e00014f8748700b07135589
Sylvain Guérin <sylvain@openflexo.org>
2017-05-16 15:33:16 +0200

    MEDIUM / Prevent NPE

--- flexo-foundation/src/main/java/org/openflexo/foundation/fml/rm/AbstractVirtualModelResourceImpl.java
+++ flexo-foundation/src/main/java/org/openflexo/foundation/fml/rm/AbstractVirtualModelResourceImpl.java
@@ -103,8 +103,9 @@ public abstract class AbstractVirtualModelResourceImpl<VM extends AbstractVirtua
 	public void stopDeserializing() {
 		// NPE Protection and warning
 		VM data = getLoadedResourceData();
-		data.finalizeDeserialization();
-
+		if (data != null) {
+			data.finalizeDeserialization();
+		}
 		if (data == null) {
 			logger.warning("INVESTIGATE: NO DATA has been derserialized from VirtualModelResource - " + this.getURI());
 		}
