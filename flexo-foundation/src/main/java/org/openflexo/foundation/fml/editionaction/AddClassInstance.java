3b994e20fce980c0be094fee16b66ac1fb7a15aa
Sylvain Guérin <sylvain@openflexo.org>
2017-06-22 22:09:34 +0200

    INFO / Improved FML representation for AddClassInstance

--- flexo-foundation/src/main/java/org/openflexo/foundation/fml/editionaction/AddClassInstance.java
+++ flexo-foundation/src/main/java/org/openflexo/foundation/fml/editionaction/AddClassInstance.java
@@ -50,6 +50,7 @@ import org.openflexo.connie.DataBinding.BindingDefinitionType;
 import org.openflexo.connie.exception.NullReferenceException;
 import org.openflexo.connie.exception.TypeMismatchException;
 import org.openflexo.connie.type.TypeUtils;
+import org.openflexo.foundation.fml.FMLRepresentationContext;
 import org.openflexo.foundation.fml.annotations.FML;
 import org.openflexo.foundation.fml.editionaction.AddClassInstance.AddClassInstanceImpl;
 import org.openflexo.foundation.fml.rt.RunTimeEvaluationContext;
@@ -149,6 +150,16 @@ public interface AddClassInstance extends AssignableAction<Object> {
 		public void createParameter() {
 			DataBinding<Object> aParameter = new DataBinding<>("\"value\"", this, Object.class, BindingDefinitionType.GET);
 			addToParameters(aParameter);
+		}
+
+		@Override
+		public String getFMLRepresentation(FMLRepresentationContext context) {
+			return "new " + TypeUtils.simpleRepresentation(getType()) + "()";
+		}
+
+		@Override
+		public String getStringRepresentation() {
+			return "new " + TypeUtils.simpleRepresentation(getType()) + "()";
 		}
 	}
 
