84a0ce325f88cb3a78aeca7b72329c22572934d3
Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
2019-02-11 09:50:36 +0100

    LOW / typos and less warning

--- flexo-foundation/src/main/java/org/openflexo/foundation/technologyadapter/ModelSlot.java
+++ flexo-foundation/src/main/java/org/openflexo/foundation/technologyadapter/ModelSlot.java
@@ -144,7 +144,7 @@ public interface ModelSlot<RD extends ResourceData<RD> & TechnologyObject<?>> ex
 
 	/**
 	 * Creates and return a new {@link FlexoRole} of supplied class.<br>
-	 * This responsability is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
+	 * This responsibility is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
 	 * {@link FlexoRole} types
 	 * 
 	 * @param flexoRoleClass
@@ -154,7 +154,7 @@ public interface ModelSlot<RD extends ResourceData<RD> & TechnologyObject<?>> ex
 
 	/**
 	 * Creates and return a new {@link EditionAction} of supplied class.<br>
-	 * This responsability is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
+	 * This responsibility is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
 	 * {@link EditionAction} types
 	 * 
 	 * @param editionActionClass
@@ -164,7 +164,7 @@ public interface ModelSlot<RD extends ResourceData<RD> & TechnologyObject<?>> ex
 
 	/**
 	 * Creates and return a new {@link AbstractFetchRequest} of supplied class.<br>
-	 * This responsability is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
+	 * This responsibility is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
 	 * {@link AbstractFetchRequest} types
 	 * 
 	 * @param fetchRequestClass
@@ -216,7 +216,7 @@ public interface ModelSlot<RD extends ResourceData<RD> & TechnologyObject<?>> ex
 
 		private boolean isRequired;
 		private boolean isReadOnly;
-		private TechnologyAdapter technologyAdapter;
+		private TechnologyAdapter<?> technologyAdapter;
 
 		/*private List<Class<? extends FlexoRole<?>>> availableFlexoRoleTypes;
 		private List<Class<? extends FlexoBehaviour>> availableFlexoBehaviourTypes;
@@ -232,7 +232,7 @@ public interface ModelSlot<RD extends ResourceData<RD> & TechnologyObject<?>> ex
 
 		/**
 		 * Creates and return a new {@link FlexoRole} of supplied class.<br>
-		 * This responsability is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
+		 * This responsibility is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
 		 * {@link FlexoRole} types
 		 * 
 		 * @param flexoRoleClass
@@ -381,7 +381,7 @@ public interface ModelSlot<RD extends ResourceData<RD> & TechnologyObject<?>> ex
 
 		/**
 		 * Creates and return a new {@link EditionAction} of supplied class.<br>
-		 * This responsability is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
+		 * This responsibility is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
 		 * {@link EditionAction} types
 		 * 
 		 * @param editionActionClass
@@ -395,7 +395,7 @@ public interface ModelSlot<RD extends ResourceData<RD> & TechnologyObject<?>> ex
 
 		/**
 		 * Creates and return a new {@link AbstractFetchRequest} of supplied class.<br>
-		 * This responsability is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
+		 * This responsibility is delegated to the technology-specific {@link ModelSlot} which manages with introspection its own
 		 * {@link AbstractFetchRequest} types
 		 * 
 		 * @param fetchRequestClass
