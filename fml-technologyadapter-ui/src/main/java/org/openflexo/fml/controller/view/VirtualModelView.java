66fd7bd5e6482f95179392b3270d768c7df9161d
Sylvain Guérin <sylvain@openflexo.org>
2019-05-09 17:44:34 +0200

    MEDIUM / Working on CORE-308 / Improved FML browsers while editing FML VirtualModels

--- fml-technologyadapter-ui/src/main/java/org/openflexo/fml/controller/view/VirtualModelView.java
+++ fml-technologyadapter-ui/src/main/java/org/openflexo/fml/controller/view/VirtualModelView.java
@@ -39,6 +39,7 @@
 package org.openflexo.fml.controller.view;
 
 import org.openflexo.fml.controller.CommonFIB;
+import org.openflexo.fml.controller.widget.FIBVirtualModelBrowser;
 import org.openflexo.foundation.fml.VirtualModel;
 import org.openflexo.rm.Resource;
 import org.openflexo.view.controller.FlexoController;
@@ -52,12 +53,22 @@ import org.openflexo.view.controller.model.FlexoPerspective;
  */
 public abstract class VirtualModelView extends FlexoConceptView<VirtualModel> {
 
+	private FIBVirtualModelBrowser browser;
+
 	public VirtualModelView(VirtualModel virtualModel, FlexoController controller, FlexoPerspective perspective) {
 		super(virtualModel, CommonFIB.VIRTUAL_MODEL_VIEW_FIB, controller, perspective);
+		browser = new FIBVirtualModelBrowser(virtualModel, perspective.getController());
 	}
 
 	public VirtualModelView(VirtualModel virtualModel, Resource fibFile, FlexoController controller, FlexoPerspective perspective) {
 		super(virtualModel, fibFile, controller, perspective);
+		browser = new FIBVirtualModelBrowser(virtualModel, perspective.getController());
+	}
+
+	@Override
+	public void willShow() {
+		super.willShow();
+		getPerspective().setBottomLeftView(browser);
 	}
 
 }
