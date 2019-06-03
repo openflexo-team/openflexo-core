beeee37a801ddac0778285786764b9ebe8cca4d5
Sylvain Guérin <sylvain@openflexo.org>
2019-06-03 12:13:18 +0200

    MEDIUM / Fixed issue with computation of ModelSlot with multiple cardinality

--- flexo-foundation/src/main/java/org/openflexo/foundation/fml/binding/VirtualModelModelSlotPathElement.java
+++ flexo-foundation/src/main/java/org/openflexo/foundation/fml/binding/VirtualModelModelSlotPathElement.java
@@ -38,72 +38,20 @@
 
 package org.openflexo.foundation.fml.binding;
 
-import java.lang.reflect.Type;
 import java.util.logging.Logger;
 
-import org.openflexo.connie.BindingEvaluationContext;
 import org.openflexo.connie.binding.IBindingPathElement;
-import org.openflexo.connie.binding.SimplePathElement;
-import org.openflexo.connie.exception.NullReferenceException;
-import org.openflexo.connie.exception.TypeMismatchException;
-import org.openflexo.foundation.fml.rt.FMLRTVirtualModelInstance;
-import org.openflexo.foundation.fml.rt.ModelSlotInstance;
-import org.openflexo.foundation.fml.rt.VirtualModelInstance;
 import org.openflexo.foundation.technologyadapter.ModelSlot;
 
-public class VirtualModelModelSlotPathElement<MS extends ModelSlot> extends SimplePathElement {
+public class VirtualModelModelSlotPathElement<MS extends ModelSlot<?>> extends FlexoConceptFlexoPropertyPathElement<MS> {
 
 	private static final Logger logger = Logger.getLogger(VirtualModelModelSlotPathElement.class.getPackage().getName());
 
-	private final MS modelSlot;
-
 	public VirtualModelModelSlotPathElement(IBindingPathElement parent, MS modelSlot) {
-		super(parent, modelSlot.getName(), modelSlot.getResultingType());
-		this.modelSlot = modelSlot;
+		super(parent, modelSlot);
 	}
 
 	public MS getModelSlot() {
-		return modelSlot;
+		return getFlexoProperty();
 	}
-
-	@Override
-	public String getLabel() {
-		return modelSlot.getName();
-	}
-
-	@Override
-	public String getTooltipText(Type resultingType) {
-		return modelSlot.getDescription();
-	}
-
-	@Override
-	public Object getBindingValue(Object target, BindingEvaluationContext context) throws TypeMismatchException, NullReferenceException {
-		if (target instanceof FMLRTVirtualModelInstance) {
-			VirtualModelInstance<?, ?> vmi = (VirtualModelInstance<?, ?>) target;
-			ModelSlotInstance<?, ?> msi = vmi.getModelSlotInstance(modelSlot);
-			if (msi != null) {
-				return msi.getAccessedResourceData();
-			}
-			return null;
-		}
-		logger.warning("Please implement me, modelSlot=" + modelSlot + " target=" + target + " context=" + context);
-		return null;
-	}
-
-	@Override
-	public void setBindingValue(Object value, Object target, BindingEvaluationContext context)
-			throws TypeMismatchException, NullReferenceException {
-
-		if (target instanceof FMLRTVirtualModelInstance) {
-
-			System.out.println("OK, on tente de mettre la valeur suivante a " + modelSlot + " : " + value);
-
-			VirtualModelInstance<?, ?> vmi = (FMLRTVirtualModelInstance) target;
-			vmi.setFlexoPropertyValue(modelSlot, value);
-			return;
-		}
-
-		logger.warning("Please implement me, modelSlot=" + modelSlot + " target=" + target + " context=" + context);
-	}
-
 }
--- flexo-foundation/src/main/java/org/openflexo/foundation/fml/rt/VirtualModelInstance.java
+++ flexo-foundation/src/main/java/org/openflexo/foundation/fml/rt/VirtualModelInstance.java
@@ -76,7 +76,6 @@ import org.openflexo.foundation.resource.CannotRenameException;
 import org.openflexo.foundation.resource.FlexoResource;
 import org.openflexo.foundation.technologyadapter.FlexoModel;
 import org.openflexo.foundation.technologyadapter.ModelRepository;
-import org.openflexo.foundation.technologyadapter.ModelSlot;
 import org.openflexo.foundation.technologyadapter.TechnologyAdapter;
 import org.openflexo.foundation.technologyadapter.TechnologyObject;
 import org.openflexo.pamela.annotations.Adder;
@@ -964,20 +963,6 @@ public interface VirtualModelInstance<VMI extends VirtualModelInstance<VMI, TA>,
 		@Override
 		public VMI getResourceData() {
 			return (VMI) this;
-		}
-
-		@Override
-		public <T> T getFlexoActor(FlexoRole<T> flexoRole) {
-			if (flexoRole instanceof ModelSlot) {
-				ModelSlotInstance<?, ?> modelSlotInstance = getModelSlotInstance((ModelSlot) flexoRole);
-				if (modelSlotInstance != null) {
-					return (T) modelSlotInstance.getAccessedResourceData();
-				}
-			}
-			if (super.getFlexoActor(flexoRole) != null) {
-				return super.getFlexoActor(flexoRole);
-			}
-			return null;
 		}
 
 		// ==========================================================================
