9d8bcdc6bf73428f73be4344ff043137db4dc3d1
Sylvain Guérin <sylvain@openflexo.org>
2019-06-12 09:04:38 +0200

    LOW / Icon

--- flexo-ui/src/main/java/org/openflexo/view/controller/TechnologyAdapterController.java
+++ flexo-ui/src/main/java/org/openflexo/view/controller/TechnologyAdapterController.java
@@ -80,6 +80,7 @@ import org.openflexo.foundation.fml.rt.editionaction.FireEventAction;
 import org.openflexo.foundation.fml.rt.editionaction.InitiateMatching;
 import org.openflexo.foundation.fml.rt.editionaction.MatchFlexoConceptInstance;
 import org.openflexo.foundation.fml.rt.editionaction.SelectFlexoConceptInstance;
+import org.openflexo.foundation.fml.rt.editionaction.SelectVirtualModelInstance;
 import org.openflexo.foundation.nature.ProjectNature;
 import org.openflexo.foundation.nature.ProjectNatureService;
 import org.openflexo.foundation.resource.ResourceData;
@@ -373,6 +374,9 @@ public abstract class TechnologyAdapterController<TA extends TechnologyAdapter<T
 		}
 		else if (SelectFlexoConceptInstance.class.isAssignableFrom(editionActionClass)) {
 			return IconFactory.getImageIcon(FMLRTIconLibrary.FLEXO_CONCEPT_INSTANCE_ICON, IconLibrary.IMPORT);
+		}
+		else if (SelectVirtualModelInstance.class.isAssignableFrom(editionActionClass)) {
+			return IconFactory.getImageIcon(FMLRTIconLibrary.VIRTUAL_MODEL_INSTANCE_ICON, IconLibrary.IMPORT);
 		}
 		else if (InitiateMatching.class.isAssignableFrom(editionActionClass)) {
 			return IconFactory.getImageIcon(FMLRTIconLibrary.FLEXO_CONCEPT_INSTANCE_ICON, IconLibrary.SYNC);
