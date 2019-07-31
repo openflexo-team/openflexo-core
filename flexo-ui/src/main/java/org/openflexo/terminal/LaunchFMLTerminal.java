4bbd2bc6bcc64e9e7eb52264b2855f0ac905e187
sylvain <sylvain@pc50.home>
2019-07-31 15:00:11 +0200

    LOW / Formatting

--- flexo-ui/src/main/java/org/openflexo/terminal/LaunchFMLTerminal.java
+++ flexo-ui/src/main/java/org/openflexo/terminal/LaunchFMLTerminal.java
@@ -37,13 +37,15 @@ import org.openflexo.foundation.utils.ProjectLoadingCancelledException;
 import org.openflexo.logging.FlexoLoggingManager;
 
 /**
- * Launching in a terminal: cd /Users/sylvain/GIT-1.9.0/openflexo-core/fml-cli/bin/main
+ * Launching in a terminal: cd
+ * /Users/sylvain/GIT-1.9.0/openflexo-core/fml-cli/bin/main
  * 
  * java org.openflexo.foundation.fml.cli.LaunchHeadlessFMLCLI
  * 
  * or
  * 
- * java -cp "Java/*" org.openflexo.foundation.fml.cli.LaunchHeadlessFMLCLI from .app
+ * java -cp "Java/*" org.openflexo.foundation.fml.cli.LaunchHeadlessFMLCLI from
+ * .app
  * 
  * 
  * @author sylvain
@@ -65,9 +67,10 @@ public class LaunchFMLTerminal {
 	public static FlexoServiceManager createServiceManager(Options options) {
 		FlexoServiceManager manager = new DefaultFlexoServiceManager(null, options.devMode);
 		TechnologyAdapterService technologyAdapterService = manager.getTechnologyAdapterService();
-		technologyAdapterService.activateTechnologyAdapter(technologyAdapterService.getTechnologyAdapter(FMLTechnologyAdapter.class), true);
-		technologyAdapterService.activateTechnologyAdapter(technologyAdapterService.getTechnologyAdapter(FMLRTTechnologyAdapter.class),
-				true);
+		technologyAdapterService.activateTechnologyAdapter(
+				technologyAdapterService.getTechnologyAdapter(FMLTechnologyAdapter.class), true);
+		technologyAdapterService.activateTechnologyAdapter(
+				technologyAdapterService.getTechnologyAdapter(FMLRTTechnologyAdapter.class), true);
 		return manager;
 	}
 
@@ -92,52 +95,49 @@ public class LaunchFMLTerminal {
 		for (int i = 0; i < args.length; i++) {
 			String arg = args[i];
 			switch (arg) {
-				case "--help":
-					usage();
-					break;
-				case "--verbose":
-					options.verbose = true;
-					break;
-				case "--rc":
-					if (i + 1 < args.length) {
-						options.rcPaths.add(args[++i]);
-					}
-					else {
-						System.err.println("Option " + arg + " needs an argument.");
-						System.exit(1);
-					}
-					break;
-				case "--p":
-					if (i + 1 < args.length) {
-						options.projectPath = args[++i];
-					}
-					else {
-						System.err.println("Option " + arg + " needs an argument.");
-						System.exit(1);
-					}
-					break;
-				default: {
-					if (arg.length() >= 2 && arg.charAt(0) == '-' && arg.charAt(1) != '-') {
-						for (int j = 1; j < arg.length(); j++) {
-							switch (arg.charAt(j)) {
-								case 'h':
-									usage();
-									break;
-								case 'v':
-									options.verbose = true;
-									break;
-								default:
-									System.err.println("Unknown short option '" + arg.charAt(j) + "'");
-									System.exit(1);
+			case "--help":
+				usage();
+				break;
+			case "--verbose":
+				options.verbose = true;
+				break;
+			case "--rc":
+				if (i + 1 < args.length) {
+					options.rcPaths.add(args[++i]);
+				} else {
+					System.err.println("Option " + arg + " needs an argument.");
+					System.exit(1);
+				}
+				break;
+			case "--p":
+				if (i + 1 < args.length) {
+					options.projectPath = args[++i];
+				} else {
+					System.err.println("Option " + arg + " needs an argument.");
+					System.exit(1);
+				}
+				break;
+			default: {
+				if (arg.length() >= 2 && arg.charAt(0) == '-' && arg.charAt(1) != '-') {
+					for (int j = 1; j < arg.length(); j++) {
+						switch (arg.charAt(j)) {
+						case 'h':
+							usage();
+							break;
+						case 'v':
+							options.verbose = true;
+							break;
+						default:
+							System.err.println("Unknown short option '" + arg.charAt(j) + "'");
+							System.exit(1);
 
-							}
 						}
 					}
-					else {
-						System.err.println("Unknown long option '" + arg + "'");
-						System.exit(1);
-					}
+				} else {
+					System.err.println("Unknown long option '" + arg + "'");
+					System.exit(1);
 				}
+			}
 
 			}
 		}
@@ -155,8 +155,14 @@ public class LaunchFMLTerminal {
 
 		for (String path : options.rcPaths) {
 			FlexoResourceCenterService centerService = serviceManager.getResourceCenterService();
-			DirectoryResourceCenter center = DirectoryResourceCenter.instanciateNewDirectoryResourceCenter(new File(path), centerService);
-			centerService.addToResourceCenters(center);
+			File rcDir = new File(path);
+			if (rcDir.exists()) {
+				DirectoryResourceCenter center = DirectoryResourceCenter.instanciateNewDirectoryResourceCenter(rcDir,
+						centerService);
+				centerService.addToResourceCenters(center);
+			} else {
+				System.err.println("Directory not found: '" + rcDir + "'");
+			}
 		}
 
 		// Settings.getInstance().setAnsiConsole(false);
@@ -184,15 +190,11 @@ public class LaunchFMLTerminal {
 				(options.projectPath != null ? projectDirectory : new File(System.getProperty("user.dir"))));
 		terminal.open(100, 100, 1024, 700);
 
-		/*} catch (Exception e) {
-			System.out.println("Caught an Exception :");
-			e.printStackTrace();
-			try {
-				System.out.println("Press enter to continue.");
-				int c = System.in.read();
-			} catch (IOException xx) {
-			}
-		}*/
+		/*
+		 * } catch (Exception e) { System.out.println("Caught an Exception :");
+		 * e.printStackTrace(); try { System.out.println("Press enter to continue.");
+		 * int c = System.in.read(); } catch (IOException xx) { } }
+		 */
 
 	}
 
