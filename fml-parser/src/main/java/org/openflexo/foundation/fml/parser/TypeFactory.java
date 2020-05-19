7c33b402804087ef3aee4826286bc823c7bdeef2
Sylvain GUERIN <sylvain.guerin@openflexo.org>
2020-05-19 16:16:26 +0200

    MEDIUM / Prevent adding validation issue twice

--- fml-parser/src/main/java/org/openflexo/foundation/fml/parser/TypeFactory.java
+++ fml-parser/src/main/java/org/openflexo/foundation/fml/parser/TypeFactory.java
@@ -126,6 +126,40 @@ public class TypeFactory extends SemanticsAnalyzerFactory {
 			this.fragment = fragment;
 			this.type = type;
 		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((fragment == null) ? 0 : fragment.hashCode());
+			result = prime * result + ((type == null) ? 0 : type.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			UnresolvedTypeReference other = (UnresolvedTypeReference) obj;
+			if (fragment == null) {
+				if (other.fragment != null)
+					return false;
+			}
+			else if (!fragment.equals(other.fragment))
+				return false;
+			if (type == null) {
+				if (other.type != null)
+					return false;
+			}
+			else if (!type.equals(other.type))
+				return false;
+			return true;
+		}
+
 	}
 
 	public TypeFactory(MainSemanticsAnalyzer analyzer) {
@@ -208,7 +242,15 @@ public class TypeFactory extends SemanticsAnalyzerFactory {
 
 			}
 		};
-		unresolvedTypes = new ArrayList<>();
+		unresolvedTypes = new ArrayList<UnresolvedTypeReference>() {
+			@Override
+			public boolean add(UnresolvedTypeReference ref) {
+				if (!contains(ref)) {
+					return super.add(ref);
+				}
+				return false;
+			}
+		};
 	}
 
 	public void resolveUnresovedTypes() {
