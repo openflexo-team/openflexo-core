f6e30ded99f9d3df5a203952cff86bb1549de0eb
Sylvain GUERIN <sylvain.guerin@openflexo.org>
2022-06-25 12:02:32 +0200

    MEDIUM / Test on FML scripts

--- fml-cli/src/test/java/org/openflexo/foundation/fml/cli/TestFMLScript2.java
+++ fml-cli/src/test/java/org/openflexo/foundation/fml/cli/TestFMLScript2.java
@@ -40,6 +40,7 @@ package org.openflexo.foundation.fml.cli;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
 import java.util.logging.Logger;
@@ -48,9 +49,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openflexo.foundation.DefaultFlexoEditor;
 import org.openflexo.foundation.FlexoEditor;
-import org.openflexo.foundation.fml.cli.command.AbstractCommand;
 import org.openflexo.foundation.fml.cli.command.AbstractCommand.ExecutionException;
 import org.openflexo.foundation.fml.cli.command.FMLScript;
+import org.openflexo.foundation.fml.cli.command.fml.FMLAssertExpression;
+import org.openflexo.foundation.fml.cli.command.fml.FMLAssignation;
+import org.openflexo.foundation.fml.cli.command.fml.FMLContextCommand;
 import org.openflexo.foundation.resource.FlexoResourceCenter;
 import org.openflexo.foundation.resource.FlexoResourceCenterService;
 import org.openflexo.pamela.exceptions.ModelDefinitionException;
@@ -66,9 +69,9 @@ import org.openflexo.test.TestOrder;
  *
  */
 @RunWith(OrderedRunner.class)
-public class TestFMLScript1 extends FMLScriptParserTestCase {
+public class TestFMLScript2 extends FMLScriptParserTestCase {
 
-	private static final Logger logger = Logger.getLogger(TestFMLScript1.class.getPackage().getName());
+	private static final Logger logger = Logger.getLogger(TestFMLScript2.class.getPackage().getName());
 
 	static FlexoEditor editor;
 
@@ -81,28 +84,18 @@ public class TestFMLScript1 extends FMLScriptParserTestCase {
 	@Test
 	@TestOrder(1)
 	public void initServiceManager() throws ParseException, ModelDefinitionException, IOException {
-
-		// System.out.println("Prout: " + new File(System.getProperty("user.dir")));
-		// System.out.println("Home: " + HOME_DIR);
-		// System.exit(-1);
-
 		instanciateTestServiceManager();
 
 		editor = new DefaultFlexoEditor(null, serviceManager);
 		assertNotNull(editor);
 
-		commandInterpreter = new CommandInterpreter(serviceManager, System.in, System.out, System.err,
-				HOME_DIR /*new File(System.getProperty("user.dir"))*/);
+		commandInterpreter = new CommandInterpreter(serviceManager, System.in, System.out, System.err, HOME_DIR);
 
 		rcService = commandInterpreter.getServiceManager().getResourceCenterService();
 		FlexoResourceCenter<?> existingResourcesRC = rcService.getFlexoResourceCenter("http://openflexo.org/test/flexo-test-resources");
 		logger.info("Copying all files from " + existingResourcesRC);
 		testResourcesRC = makeNewDirectoryResourceCenterFromExistingResourceCenter(serviceManager, existingResourcesRC);
 		logger.info("Now working with " + testResourcesRC);
-
-		System.out.println("Working from " + commandInterpreter.getWorkingDirectory());
-		// System.exit(-1);
-
 	}
 
 	@Test
@@ -110,7 +103,7 @@ public class TestFMLScript1 extends FMLScriptParserTestCase {
 	public void loadScript() throws ParseException, ModelDefinitionException, IOException {
 		log("Load script");
 
-		final Resource fmlFile = ResourceLocator.locateResource("TestFMLScript1.fmlscript");
+		final Resource fmlFile = ResourceLocator.locateResource("TestFMLScript2.fmlscript");
 
 		// System.out.println(FileUtils.fileContents(((FileResourceImpl) fmlFile).getFile()));
 
@@ -123,14 +116,57 @@ public class TestFMLScript1 extends FMLScriptParserTestCase {
 	public void checkScript() throws ParseException, ModelDefinitionException, IOException {
 		log("Check script");
 
-		checkFMLScript("TestFMLScript1.fmlscript", script);
+		// checkFMLScript("TestFMLScript2.fmlscript", script);
 
-		assertEquals(19, script.getCommands().size());
-		for (AbstractCommand command : script.getCommands()) {
+		assertEquals(6, script.getCommands().size());
+		/*for (AbstractCommand command : script.getCommands()) {
 			System.out.println("Check " + command + " with " + command.getNode() + " of " + command.getNode().getClass());
 			assertEquals(command.getOriginalCommandAsString(), command.toString());
 			System.out.println(">>> " + command.getOriginalCommandAsString());
-		}
+		}*/
+
+		FMLContextCommand context1 = (FMLContextCommand) script.getCommands().get(0);
+		FMLAssignation assignation1 = (FMLAssignation) script.getCommands().get(1);
+		FMLAssignation assignation2 = (FMLAssignation) script.getCommands().get(2);
+		FMLContextCommand context2 = (FMLContextCommand) script.getCommands().get(3);
+		FMLAssignation assignation3 = (FMLAssignation) script.getCommands().get(4);
+		FMLAssertExpression assertExpression = (FMLAssertExpression) script.getCommands().get(5);
+
+		assertEquals(context1.getParentCommand(), null);
+		assertEquals(assignation1.getParentCommand(), context1);
+		assertEquals(assignation2.getParentCommand(), assignation1);
+		assertEquals(context2.getParentCommand(), assignation2);
+		assertEquals(assignation3.getParentCommand(), context2);
+		assertEquals(assertExpression.getParentCommand(), assignation3);
+
+		System.out.println("BM0: " + context1.getBindingModel());
+		System.out.println("BM1: " + assignation1.getBindingModel());
+		System.out.println("BM2: " + assignation2.getBindingModel());
+		System.out.println("BM3: " + context2.getBindingModel());
+		System.out.println("BM4: " + assignation3.getBindingModel());
+		System.out.println("BM5: " + assertExpression.getBindingModel());
+
+		assertNull(context1.getBindingModel().bindingVariableNamed("a"));
+		assertNull(assignation1.getBindingModel().bindingVariableNamed("a"));
+		assertNotNull(assignation2.getBindingModel().bindingVariableNamed("a"));
+		assertNotNull(context2.getBindingModel().bindingVariableNamed("a"));
+		assertNotNull(assignation3.getBindingModel().bindingVariableNamed("a"));
+		assertNotNull(assertExpression.getBindingModel().bindingVariableNamed("a"));
+
+		assertNull(context1.getBindingModel().bindingVariableNamed("b"));
+		assertNull(assignation1.getBindingModel().bindingVariableNamed("b"));
+		assertNull(assignation2.getBindingModel().bindingVariableNamed("b"));
+		assertNotNull(context2.getBindingModel().bindingVariableNamed("b"));
+		assertNotNull(assignation3.getBindingModel().bindingVariableNamed("b"));
+		assertNotNull(assertExpression.getBindingModel().bindingVariableNamed("b"));
+
+		assertNull(context1.getBindingModel().bindingVariableNamed("c"));
+		assertNull(assignation1.getBindingModel().bindingVariableNamed("c"));
+		assertNull(assignation2.getBindingModel().bindingVariableNamed("c"));
+		assertNull(context2.getBindingModel().bindingVariableNamed("c"));
+		assertNull(assignation3.getBindingModel().bindingVariableNamed("c"));
+		assertNotNull(assertExpression.getBindingModel().bindingVariableNamed("c"));
+
 	}
 
 	@Test
