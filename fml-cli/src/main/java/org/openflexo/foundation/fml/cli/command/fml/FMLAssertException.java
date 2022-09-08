a4127ec636c66dde5385e7e8deb20bee926b1a19
Sylvain Guérin <sylvain.guerin@ensta-bretagne.org>
2022-09-08 23:08:14 +0200

    MEDIUM / Support for line numbers in FML scripts + improved error message when assertion fails

--- fml-cli/src/main/java/org/openflexo/foundation/fml/cli/command/fml/FMLAssertException.java
+++ fml-cli/src/main/java/org/openflexo/foundation/fml/cli/command/fml/FMLAssertException.java
@@ -4,9 +4,10 @@ import org.openflexo.connie.DataBinding;
 import org.openflexo.connie.exception.TransformException;
 import org.openflexo.connie.expr.BinaryOperatorExpression;
 import org.openflexo.connie.expr.OperatorNotSupportedException;
-import org.openflexo.connie.java.expr.JavaExpressionEvaluator;
 import org.openflexo.foundation.fml.cli.AbstractCommandInterpreter;
 import org.openflexo.foundation.fml.cli.command.FMLCommandExecutionException;
+import org.openflexo.foundation.fml.expr.FMLConstant;
+import org.openflexo.foundation.fml.expr.FMLExpressionEvaluator;
 import org.openflexo.foundation.fml.expr.FMLPrettyPrinter;
 
 /**
@@ -23,32 +24,45 @@ public class FMLAssertException extends FMLCommandExecutionException {
 	private BinaryOperatorExpression binOp;
 	private Object leftV;
 	private Object rightV;
+	private int line;
+	private String message;
 
-	public FMLAssertException(DataBinding<?> expression, AbstractCommandInterpreter commandInterpreter) {
-		super("Assert failed: " + expression);
+	public FMLAssertException(int line, DataBinding<?> expression, AbstractCommandInterpreter commandInterpreter) {
+		super("Line " + line + ": assert failed: " + expression);
+		this.line = line;
 		if (expression.getExpression() instanceof BinaryOperatorExpression) {
 			// In this case, we try to get a more explicit message
 			try {
 				binOp = (BinaryOperatorExpression) expression.getExpression();
-				leftV = binOp.getLeftArgument().transform(new JavaExpressionEvaluator(commandInterpreter));
-				rightV = binOp.getRightArgument().transform(new JavaExpressionEvaluator(commandInterpreter));
+				leftV = binOp.getLeftArgument().transform(new FMLExpressionEvaluator(commandInterpreter));
+				rightV = binOp.getRightArgument().transform(new FMLExpressionEvaluator(commandInterpreter));
 			} catch (TransformException e) {
 				e.printStackTrace();
 			}
+
+			String leftMessage = binOp.getLeftArgument().toString();
+			if (!(binOp.getLeftArgument() instanceof FMLConstant)) {
+				leftMessage += "[" + leftV + "]";
+			}
+			String rightMessage = binOp.getRightArgument().toString();
+			if (!(binOp.getRightArgument() instanceof FMLConstant)) {
+				rightMessage += "[" + rightV + "]";
+			}
+
+			try {
+				message = "Line " + line + ": assert failed: " + leftMessage + " "
+						+ FMLPrettyPrinter.getInstance().getGrammar().getSymbol(binOp.getOperator()) + " " + rightMessage;
+			} catch (OperatorNotSupportedException e) {
+				message = "Line " + line + ": assert failed: " + expression;
+				e.printStackTrace();
+			}
+
 		}
 	}
 
 	@Override
 	public String getMessage() {
-		if (binOp != null) {
-			try {
-				return "Assert failed: " + leftV + " " + FMLPrettyPrinter.getInstance().getGrammar().getSymbol(binOp.getOperator()) + " "
-						+ rightV;
-			} catch (OperatorNotSupportedException e) {
-				e.printStackTrace();
-			}
-		}
-		return super.getMessage();
+		return message;
 	}
 
 }
