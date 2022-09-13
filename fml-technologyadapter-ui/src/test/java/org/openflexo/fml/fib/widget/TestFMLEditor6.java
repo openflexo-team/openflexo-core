c21afe71179e2db72a28a7597f53065539b9d3f2
Sylvain Guérin <sylvain.guerin@ensta-bretagne.org>
2022-09-13 16:34:09 +0200

    MEDIUM / New unit test

---
+++ fml-technologyadapter-ui/src/test/java/org/openflexo/fml/fib/widget/TestFMLEditor6.java
@@ -0,0 +1,303 @@
+/**
+ * 
+ * Copyright (c) 2014-2015, Openflexo
+ * 
+ * This file is part of Fml-technologyadapter-ui, a component of the software infrastructure 
+ * developed at Openflexo.
+ * 
+ * 
+ * Openflexo is dual-licensed under the European Union Public License (EUPL, either 
+ * version 1.1 of the License, or any later version ), which is available at 
+ * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ * and the GNU General Public License (GPL, either version 3 of the License, or any 
+ * later version), which is available at http://www.gnu.org/licenses/gpl.html .
+ * 
+ * You can redistribute it and/or modify under the terms of either of these licenses
+ * 
+ * If you choose to redistribute it and/or modify under the terms of the GNU GPL, you
+ * must include the following additional permission.
+ *
+ *          Additional permission under GNU GPL version 3 section 7
+ *
+ *          If you modify this Program, or any covered work, by linking or 
+ *          combining it with software containing parts covered by the terms 
+ *          of EPL 1.0, the licensors of this Program grant you additional permission
+ *          to convey the resulting work. * 
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. 
+ *
+ * See http://www.openflexo.org/license.html for details.
+ * 
+ * 
+ * Please contact Openflexo (openflexo-contacts@openflexo.org)
+ * or visit www.openflexo.org if you need additional information.
+ * 
+ */
+
+package org.openflexo.fml.fib.widget;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.openflexo.fml.controller.FMLFIBController;
+import org.openflexo.fml.controller.widget.fmleditor.FMLEditor;
+import org.openflexo.foundation.FlexoEditor;
+import org.openflexo.foundation.FlexoException;
+import org.openflexo.foundation.fml.FMLCompilationUnit;
+import org.openflexo.foundation.fml.FlexoConcept;
+import org.openflexo.foundation.fml.VirtualModel;
+import org.openflexo.foundation.fml.VirtualModelLibrary;
+import org.openflexo.foundation.fml.rm.CompilationUnitResource;
+import org.openflexo.foundation.resource.ResourceLoadingCancelledException;
+import org.openflexo.gina.ApplicationFIBLibrary.ApplicationFIBLibraryImpl;
+import org.openflexo.gina.swing.utils.FIBJPanel;
+import org.openflexo.gina.test.OpenflexoFIBTestCase;
+import org.openflexo.gina.test.SwingGraphicalContextDelegate;
+import org.openflexo.gina.utils.InspectorGroup;
+import org.openflexo.pamela.validation.ValidationReport;
+import org.openflexo.rm.Resource;
+import org.openflexo.rm.ResourceLocator;
+import org.openflexo.test.OrderedRunner;
+import org.openflexo.test.TestOrder;
+import org.openflexo.test.UITest;
+
+/**
+ * Test {@link FMLEditor} component
+ * 
+ * @author sylvain
+ * 
+ */
+@RunWith(OrderedRunner.class)
+public class TestFMLEditor6 extends OpenflexoFIBTestCase {
+
+	private static SwingGraphicalContextDelegate gcDelegate;
+
+	private static Resource fibResource;
+
+	static FlexoEditor editor;
+
+	@BeforeClass
+	public static void setupClass() {
+		instanciateTestServiceManager();
+		initGUI();
+	}
+
+	private static CompilationUnitResource fmlResource;
+
+	private static FMLCompilationUnit compilationUnit;
+	private static VirtualModel virtualModel;
+
+	private static FlexoConcept foo;
+	private static FlexoConcept foo2;
+
+	@Test
+	@TestOrder(3)
+	@Category(UITest.class)
+	public void loadFMLResource() throws FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+
+		VirtualModelLibrary vpLib = serviceManager.getVirtualModelLibrary();
+		assertNotNull(vpLib);
+		VirtualModel vm = vpLib.getVirtualModel("http://openflexo.org/test/TestResourceCenter/TestVirtualModelA.fml");
+		assertNotNull(vm);
+
+		fmlResource = vm.getResource();
+		assertNotNull(fmlResource);
+
+		compilationUnit = fmlResource.getCompilationUnit();
+		virtualModel = compilationUnit.getVirtualModel();
+
+	}
+
+	private static FMLEditor fmlEditor;
+
+	@Test
+	@TestOrder(4)
+	@Category(UITest.class)
+	public void testInstanciateFMLEditor() {
+
+		fmlEditor = new FMLEditor(fmlResource, null);
+		gcDelegate.addTab("FML Editor", fmlEditor);
+		FMLCompilationUnit cu = fmlEditor.getFMLResource().getCompilationUnit();
+		assertNotNull(cu);
+		assertEquals(0, cu.getVirtualModel().getFlexoProperties().size());
+		assertEquals(0, cu.getVirtualModel().getFlexoBehaviours().size());
+	}
+
+	@Test
+	@TestOrder(5)
+	@Category(UITest.class)
+	public void testInstanciateWidget() {
+
+		fibResource = ResourceLocator.locateResource("Fib/FML/CompilationUnitView.fib");
+		assertTrue(fibResource != null);
+		FIBJPanel<FMLCompilationUnit> widget = instanciateFIB(fibResource, fmlResource.getCompilationUnit(), FMLCompilationUnit.class);
+		FMLFIBController fibController = (FMLFIBController) widget.getController();
+		InspectorGroup fmlInspectorGroup = new InspectorGroup(ResourceLocator.locateResource("Inspectors/FML"),
+				ApplicationFIBLibraryImpl.instance(), null);
+		fibController.setDefaultInspectorGroup(fmlInspectorGroup);
+
+		// ModuleInspectorController inspectorController = new ModuleInspectorController(null);
+		// fibController.setInspectorController
+		gcDelegate.addTab("Standard GUI", widget.getController());
+	}
+
+	public static void initGUI() {
+		gcDelegate = new SwingGraphicalContextDelegate(TestFMLEditor6.class.getSimpleName());
+	}
+
+	@AfterClass
+	public static void waitGUI() {
+		gcDelegate.waitGUI();
+	}
+
+	@Before
+	public void setUp() {
+		gcDelegate.setUp();
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+		gcDelegate.tearDown();
+	}
+
+	@Test
+	@TestOrder(6)
+	@Category(UITest.class)
+	public void createConcept1() {
+
+		log("createConcept1");
+
+		// @formatter:off
+		String fml = "use org.openflexo.foundation.fml.rt.FMLRTVirtualModelInstanceModelSlot as FMLRT;\n\n"
+				+ "@URI(\"http://openflexo.org/test/TestResourceCenter/TestVirtualModelA.fml\")\n" 
+				+ "@Version(\"0.1\")\n"
+				+ "model TestVirtualModelA {\n" 
+				+ "  concept Foo {\n" 
+				+ "  }\n"
+				+ "}\n";
+		// @formatter:on
+
+		fmlEditor.getTextArea().setText(fml);
+		fmlEditor.parseImmediately();
+
+		FMLCompilationUnit cu = fmlEditor.getFMLResource().getCompilationUnit();
+		assertNotNull(cu);
+		assertEquals(0, cu.getVirtualModel().getFlexoProperties().size());
+		assertEquals(0, cu.getVirtualModel().getFlexoBehaviours().size());
+		assertEquals(1, cu.getVirtualModel().getFlexoConcepts().size());
+
+		assertSame(cu, compilationUnit);
+		assertSame(cu.getVirtualModel(), virtualModel);
+
+		foo = cu.getVirtualModel().getFlexoConcept("Foo");
+		System.out.println("foo: " + foo);
+
+		ValidationReport validation = validate(cu);
+		assertEquals(0, validation.getErrorsCount());
+
+	}
+
+	@Test
+	@TestOrder(7)
+	@Category(UITest.class)
+	public void createConcept2() {
+
+		log("createConcept2");
+
+		// @formatter:off
+		String fml = "use org.openflexo.foundation.fml.rt.FMLRTVirtualModelInstanceModelSlot as FMLRT;\n\n"
+				+ "@URI(\"http://openflexo.org/test/TestResourceCenter/TestVirtualModelA.fml\")\n" 
+				+ "@Version(\"0.1\")\n"
+				+ "model TestVirtualModelA {\n" 
+				+ "  concept Foo {\n" 
+				+ "  }\n"
+				+ "  concept Foo2 {\n" 
+				+ "  }\n"
+				+ "}\n";
+		// @formatter:on
+
+		fmlEditor.getTextArea().setText(fml);
+		fmlEditor.parseImmediately();
+
+		FMLCompilationUnit cu = fmlEditor.getFMLResource().getCompilationUnit();
+		assertNotNull(cu);
+		assertEquals(0, cu.getVirtualModel().getFlexoProperties().size());
+		assertEquals(0, cu.getVirtualModel().getFlexoBehaviours().size());
+		assertEquals(2, cu.getVirtualModel().getFlexoConcepts().size());
+
+		assertSame(cu, compilationUnit);
+		assertSame(cu.getVirtualModel(), virtualModel);
+
+		assertSame(foo, cu.getVirtualModel().getFlexoConcept("Foo"));
+		System.out.println("foo: " + foo);
+
+		foo2 = cu.getVirtualModel().getFlexoConcept("Foo2");
+		System.out.println("foo2: " + foo2);
+
+		assertEquals(0, foo2.getParentFlexoConcepts().size());
+
+		ValidationReport validation = validate(cu);
+		assertEquals(0, validation.getErrorsCount());
+
+	}
+
+	@Test
+	@TestOrder(8)
+	@Category(UITest.class)
+	public void updateConcept2() {
+
+		log("createConcept2");
+
+		// @formatter:off
+		String fml = "use org.openflexo.foundation.fml.rt.FMLRTVirtualModelInstanceModelSlot as FMLRT;\n\n"
+				+ "@URI(\"http://openflexo.org/test/TestResourceCenter/TestVirtualModelA.fml\")\n" 
+				+ "@Version(\"0.1\")\n"
+				+ "model TestVirtualModelA {\n" 
+				+ "  concept Foo {\n" 
+				+ "  }\n"
+				+ "  concept Foo2 extends Foo {\n" 
+				+ "  }\n"
+				+ "}\n";
+		// @formatter:on
+
+		fmlEditor.getTextArea().setText(fml);
+		fmlEditor.parseImmediately();
+
+		FMLCompilationUnit cu = fmlEditor.getFMLResource().getCompilationUnit();
+		assertNotNull(cu);
+		assertEquals(0, cu.getVirtualModel().getFlexoProperties().size());
+		assertEquals(0, cu.getVirtualModel().getFlexoBehaviours().size());
+		assertEquals(2, cu.getVirtualModel().getFlexoConcepts().size());
+
+		assertSame(cu, compilationUnit);
+		assertSame(cu.getVirtualModel(), virtualModel);
+
+		System.out.println("foo: " + foo);
+		System.out.println("foo2: " + foo2);
+		assertSame(foo, cu.getVirtualModel().getFlexoConcept("Foo"));
+		assertSame(foo2, cu.getVirtualModel().getFlexoConcept("Foo2"));
+
+		assertEquals(1, foo2.getParentFlexoConcepts().size());
+		assertTrue(foo2.getParentFlexoConcepts().contains(foo));
+		assertSame(foo, foo2.getParentFlexoConcepts().get(0));
+
+		ValidationReport validation = validate(cu);
+		assertEquals(0, validation.getErrorsCount());
+
+	}
+
+}
