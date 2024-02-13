a5823ceb10e25e439b80fc04152ef148d0c1c59c
Sylvain Guérin <sylvain.guerin@ensta-bretagne.org>
2024-02-13 12:11:25 +0100

    LOW / Test

---
+++ fml-technologyadapter-ui/src/test/java/org/openflexo/fml/fib/widget/TestFMLEditor20.java
@@ -0,0 +1,214 @@
+/**
+ * 
+ * Copyright (c) 2014-2015, Openflexo
+ * 
+ * This file is part of Fml-technologyadapter-ui, a component of the software infrastructure 
+ * developed at Openflexo.
+ * 
+ * 
+ * Openflexo is dual-licensed under the European Union Public License (EUPL, either 
+ * version 1.1 of the License, or any later version ), which is available at 
+ * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ * and the GNU General Public License (GPL, either version 3 of the License, or any 
+ * later version), which is available at http://www.gnu.org/licenses/gpl.html .
+ * 
+ * You can redistribute it and/or modify under the terms of either of these licenses
+ * 
+ * If you choose to redistribute it and/or modify under the terms of the GNU GPL, you
+ * must include the following additional permission.
+ *
+ *          Additional permission under GNU GPL version 3 section 7
+ *
+ *          If you modify this Program, or any covered work, by linking or 
+ *          combining it with software containing parts covered by the terms 
+ *          of EPL 1.0, the licensors of this Program grant you additional permission
+ *          to convey the resulting work. * 
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. 
+ *
+ * See http://www.openflexo.org/license.html for details.
+ * 
+ * 
+ * Please contact Openflexo (openflexo-contacts@openflexo.org)
+ * or visit www.openflexo.org if you need additional information.
+ * 
+ */
+
+package org.openflexo.fml.fib.widget;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.openflexo.fml.controller.FMLFIBController;
+import org.openflexo.fml.controller.widget.fmleditor.FMLEditor;
+import org.openflexo.foundation.FlexoEditor;
+import org.openflexo.foundation.FlexoException;
+import org.openflexo.foundation.fml.ActionScheme;
+import org.openflexo.foundation.fml.FMLCompilationUnit;
+import org.openflexo.foundation.fml.FMLPrettyPrintDelegate;
+import org.openflexo.foundation.fml.VirtualModel;
+import org.openflexo.foundation.fml.VirtualModelLibrary;
+import org.openflexo.foundation.fml.parser.fmlnodes.FMLCompilationUnitNode;
+import org.openflexo.foundation.fml.parser.fmlnodes.FlexoBehaviourNode;
+import org.openflexo.foundation.fml.rm.CompilationUnitResource;
+import org.openflexo.foundation.resource.ResourceLoadingCancelledException;
+import org.openflexo.gina.ApplicationFIBLibrary.ApplicationFIBLibraryImpl;
+import org.openflexo.gina.swing.utils.FIBJPanel;
+import org.openflexo.gina.test.OpenflexoFIBTestCase;
+import org.openflexo.gina.test.SwingGraphicalContextDelegate;
+import org.openflexo.gina.utils.InspectorGroup;
+import org.openflexo.p2pp.P2PPNode;
+import org.openflexo.pamela.validation.ValidationReport;
+import org.openflexo.rm.Resource;
+import org.openflexo.rm.ResourceLocator;
+import org.openflexo.test.OrderedRunner;
+import org.openflexo.test.TestOrder;
+import org.openflexo.test.UITest;
+import org.openflexo.toolbox.StringUtils;
+
+/**
+ * Test {@link FMLEditor} component
+ * 
+ * @author sylvain
+ * 
+ */
+@RunWith(OrderedRunner.class)
+public class TestFMLEditor20 extends OpenflexoFIBTestCase {
+
+	private static SwingGraphicalContextDelegate gcDelegate;
+
+	private static Resource fibResource;
+
+	static FlexoEditor editor;
+
+	@BeforeClass
+	public static void setupClass() {
+		instanciateTestServiceManager();
+		initGUI();
+	}
+
+	private static CompilationUnitResource fmlResource;
+
+	private static FMLCompilationUnit compilationUnit;
+	private static VirtualModel virtualModel;
+
+	private static FMLCompilationUnitNode rootNode;
+
+	private static ActionScheme actionScheme;
+
+	/**
+	 * Display in console AbstractSyntaxTree of supplied node
+	 * 
+	 * @param node
+	 *            node to display
+	 * @param indent
+	 *            identation level
+	 */
+	protected static void debug(P2PPNode<?, ?> node, int indent) {
+		System.out.println(StringUtils.buildWhiteSpaceIndentation(indent * 2) + " > " + node.getClass().getSimpleName() + " "
+				+ Integer.toHexString(node.hashCode()) + " from " + ((FMLPrettyPrintDelegate) node).getStartLocation() + "-"
+				+ node.getEndPosition() + " pre=" + node.getPrelude() + " post="
+				+ node.getPostlude() /*+ " astNode=" + node.getASTNode() + " of " + node.getASTNode().getClass()*/
+				+ " model=" + node.getModelObject());
+		// System.err.println(node.getLastParsed());
+		// node.getLastParsed();
+		indent++;
+		for (P2PPNode<?, ?> child : node.getChildren()) {
+			debug(child, indent);
+		}
+	}
+
+	@Test
+	@TestOrder(3)
+	@Category(UITest.class)
+	public void loadFMLResource() throws FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+
+		VirtualModelLibrary vpLib = serviceManager.getVirtualModelLibrary();
+		assertNotNull(vpLib);
+		VirtualModel vm = vpLib.getVirtualModel("http://openflexo.org/test/TestResourceCenter/TestSequence.fml");
+		assertNotNull(vm);
+
+		fmlResource = vm.getResource();
+		assertNotNull(fmlResource);
+
+		compilationUnit = fmlResource.getCompilationUnit();
+		virtualModel = compilationUnit.getVirtualModel();
+		actionScheme = (ActionScheme) virtualModel.getFlexoBehaviours().get(0);
+
+		assertNotNull(rootNode = (FMLCompilationUnitNode) compilationUnit.getPrettyPrintDelegate());
+		FlexoBehaviourNode<?, ?> behaviourNode = (FlexoBehaviourNode) actionScheme.getPrettyPrintDelegate();
+		assertSame(behaviourNode, rootNode.getObjectNode(actionScheme));
+		debug(behaviourNode, 0);
+
+	}
+
+	private static FMLEditor fmlEditor;
+
+	@Test
+	@TestOrder(4)
+	@Category(UITest.class)
+	public void testInstanciateFMLEditor() {
+
+		fmlEditor = new FMLEditor(fmlResource, null);
+		gcDelegate.addTab("FML Editor", fmlEditor);
+		FMLCompilationUnit cu = fmlEditor.getFMLResource().getCompilationUnit();
+		assertNotNull(cu);
+		assertEquals(0, cu.getVirtualModel().getFlexoProperties().size());
+		assertEquals(1, cu.getVirtualModel().getFlexoBehaviours().size());
+
+		ValidationReport validation = validate(cu);
+		assertEquals(0, validation.getErrorsCount());
+	}
+
+	@Test
+	@TestOrder(5)
+	@Category(UITest.class)
+	public void testInstanciateWidget() {
+
+		fibResource = ResourceLocator.locateResource("Fib/FML/CompilationUnitView.fib");
+		assertTrue(fibResource != null);
+		FIBJPanel<FMLCompilationUnit> widget = instanciateFIB(fibResource, fmlResource.getCompilationUnit(), FMLCompilationUnit.class);
+		FMLFIBController fibController = (FMLFIBController) widget.getController();
+		InspectorGroup fmlInspectorGroup = new InspectorGroup(ResourceLocator.locateResource("Inspectors/FML"),
+				ApplicationFIBLibraryImpl.instance(), null);
+		fibController.setDefaultInspectorGroup(fmlInspectorGroup);
+
+		// ModuleInspectorController inspectorController = new ModuleInspectorController(null);
+		// fibController.setInspectorController
+		gcDelegate.addTab("Standard GUI", widget.getController());
+	}
+
+	public static void initGUI() {
+		gcDelegate = new SwingGraphicalContextDelegate(TestFMLEditor20.class.getSimpleName());
+	}
+
+	@AfterClass
+	public static void waitGUI() {
+		gcDelegate.waitGUI();
+	}
+
+	@Before
+	public void setUp() {
+		gcDelegate.setUp();
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+		gcDelegate.tearDown();
+	}
+
+}
