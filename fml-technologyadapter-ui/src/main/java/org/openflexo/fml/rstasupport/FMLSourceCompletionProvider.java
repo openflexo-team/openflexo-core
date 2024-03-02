aa78f826b0711e45fe4aee03df9e9eb16a5ee931
Sylvain Guérin <sylvain.guerin@ensta-bretagne.org>
2024-03-02 18:44:36 +0100

    MEDIUM / Fixed infered BindingModel completion context

--- fml-technologyadapter-ui/src/main/java/org/openflexo/fml/rstasupport/FMLSourceCompletionProvider.java
+++ fml-technologyadapter-ui/src/main/java/org/openflexo/fml/rstasupport/FMLSourceCompletionProvider.java
@@ -272,6 +272,9 @@ public class FMLSourceCompletionProvider extends DefaultCompletionProvider {
 		 * @return
 		 */
 		public BindingModel getBindingModel() {
+			if (context instanceof FMLControlGraph) {
+				return ((FMLControlGraph) context).getInferedBindingModel();
+			}
 			if (context != null) {
 				return context.getBindingModel();
 			}
